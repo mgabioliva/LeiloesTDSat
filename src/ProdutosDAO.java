91e5f09b6125e37d2d22efb6ca387a5a019a5798
Gabriela Silva <mgabioliva@gmail.com>
2026-03-11 10:20:33 -0300

    Botão cadastrar corrigido e a conexão foi ajustada

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,42 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
 public class ProdutosDAO {
+  
+  Connection conn;
+  PreparedStatement st;
+  ResultSet rs;
+  
+  ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+  
+  public int cadastrarProduto (ProdutosDTO produto){
+    int status;
+    conectaDAO conexao = new conectaDAO();
+    conn = conexao.conectar();
     
-    Connection conn;
-    PreparedStatement prep;
-    ResultSet resultset;
-    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        conn = new conectaDAO().connectDB();
-        
-        
+    try {
+      st = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES(?,?,?)");
+      st.setString(1,produto.getNome());
+      st.setInt(2,produto.getValor());
+      st.setString(3,produto.getStatus());
+      status = st.executeUpdate();
+      
+      return status;
     }
+    
+    catch (SQLException e) {
+      JOptionPane.showMessageDialog(null, "Erro ao cadastar produtos" + e.getMessage());
+      return e.getErrorCode();
+    }
+  }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
         return listagem;
     }
-    
-    
-    
         
 }
 
--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -4,38 +4,31 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
-    Connection conn = null;
-    public Connection connectDB(){
-      
-        try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11","root", "2008");
-            JOptionPane.showMessageDialog(null, "Conexão com o Banco de dados foi bem sucedida");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
-        }
-        return conn;
-    }
+  Connection conn = null;
+  
+  public Connection conectar(){
     
-    public void desconectar(){
-      
-      try {
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11","root", "2008");
+      JOptionPane.showMessageDialog(null, "Conexão com o banco de dados foi bem sucedida");
+    }
+    catch (ClassNotFoundException e) {
+      JOptionPane.showMessageDialog(null,"Driver do MySQL não encontrado");
+    }
+    catch (SQLException e){
+      e.printStackTrace();
+      JOptionPane.showMessageDialog(null, "Erro ConectaDAO " + e.getMessage());
+    }
+    return conn;
+  }
+  
+  public void desconectar(){
+    try {
       conn.close();
-      } 
+    }
     catch (SQLException ex) { }
   }
-    
+
 }
